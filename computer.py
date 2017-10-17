ba8696f8568b3eaefb1cdf5bfcb2eae5abb9e141
Nei Oliveira Neto (ELO) <nei.neto@pucrs.br>
2017-10-17 19:35:28 -0200

    Added more comments and refined the architecture.

--- computer.py
+++ computer.py
@@ -6,52 +6,47 @@ from collections import deque
 
 class Computer(object):
     """xd"""
+
     def __init__(self, nickname, next_computer_address="127.0.0.1"):
         self.queue = deque()
         self.nickname = nickname
         self.udp_ip = next_computer_address
         self.udp_port = 5000
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-        
+
     def start(self):
-        self.sock.bind((udp_ip, udp_port))
+        self.sock.bind((self.udp_ip, self.udp_port))
         while True:
-            if has_token():
-                if len(queue) > 0:  # if I want to send messages
-                    # send message
-                else:
-                    # pass token
-            else:
-                # get_packet
+            # get packets from socket
+            if packet is token:
                 if nickname == packet.dest_nickname:  # if I am the destination device of this packet
                     #  mark packet as read
                     continue
                 else:
-                    #  take my IP off of packet
+                    # take my IP off of packet
                     #  write the IP of the next computer in the network
                     #  send it
-                
-                    
-
-    def has_token():
-        pass
+            # elif packet is token
+                if len(self.queue) > 0:  # if I want to send messages
+                    # send message and delete from queue (.pop())
+                # pass token
 
 
 '''
 <ip_destino_token>
 <apelido>
-<tempo_token>
+<tempo_token> ???
 '''
-     
-class Token(object):
-    """lol"""
-    def __init__(self):
-        # something that identifies on which computer am I
 
 class Packet(object):
     """Datagram."""
-    def __init__(self, dest_ip, dest_nickname, text):
+    
+    def __init__(self, dest_ip: str, dest_nickname: str, time, text: str):
         self.dest_ip = dest_ip
         self.dest_nickname = dest_nickname
+        self.time = time
         self.text = text
+
+
+    def __str__(self) -> str:
+        return "{}\n{}\n{}\n{}".format(self.dest_ip, self.dest_nickname, self.time, self.text)
