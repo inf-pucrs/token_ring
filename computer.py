93098f5fcfc9c54487888ba1ea39aee8bb517c5e
15105100 <15105100@l1726959.portoalegre.pucrsnet.br>
2017-10-18 19:01:21 -0200

    aa

--- computer.py
+++ computer.py
@@ -7,31 +7,22 @@ from collections import deque
 class Computer(object):
     """xd"""
 
-    def __init__(self, nickname, next_computer_address="127.0.0.1"):
+    def __init__(self):
         self.queue = deque()
-        self.nickname = nickname
-        self.udp_ip = next_computer_address
         self.udp_port = 5000
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def start(self):
-        self.sock.bind((self.udp_ip, self.udp_port))
-        while True:
-            # get packets from socket
-            if packet is token:
-                if nickname == packet.dest_nickname:  # if I am the destination device of this packet
-                    #  mark packet as read
-                    continue
-                else:
-                    # take my IP off of packet
-                    #  write the IP of the next computer in the network
-                    #  send it
-            # elif packet is token
-                if len(self.queue) > 0:  # if I want to send messages
-                    # send message and delete from queue (.pop())
-                # pass token
 
-
+    def connect(self,nickname, next_computer_address="127.0.0.1",next_computer_port = 5000):
+        self.sock.sendto(b"teste",(next_computer_address,next_computer_port))
+        
+        
+    def wait_connection(self):
+        print(self.sock.recv(1024))
+        
+    def create_token(self):
+        return
+        
 '''
 <ip_destino_token>
 <apelido>
@@ -50,3 +41,5 @@ class Packet(object):
 
     def __str__(self) -> str:
         return "{}\n{}\n{}\n{}".format(self.dest_ip, self.dest_nickname, self.time, self.text)
+
+        
